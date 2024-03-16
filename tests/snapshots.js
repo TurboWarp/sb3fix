8d0d6d8586c0d576c658b63a79b454c692093d8e
Muffin <muffin@mailbox.org>
2024-03-15 23:26:36 -0500

    Add snapshot testing

--- tests/snapshots.js
+++ tests/snapshots.js
@@ -1,31 +1,71 @@
 const fs = require('node:fs');
+const fsPromises = require('node:fs/promises');
 const path = require('node:path');
+const nodeTest = require('node:test');
+const assert = require('node:assert');
+const {rimraf} = require('rimraf');
+
 const sb3fix = require('../src/sb3fix');
 
-const samplesDirectory = path.join(__dirname, 'samples');
-const testFiles = fs.readdirSync(samplesDirectory).filter(i => i.endsWith('.sb3'));
+const inputDirectory = path.join(__dirname, 'samples');
+const outputDirectory = path.join(__dirname, 'expected-output');
+const testcases = fs.readdirSync(inputDirectory)
+  .filter(i => i.endsWith('.sb3'))
+  .sort()
+  .map(i => ({
+    name: i,
+    inputPath: path.join(inputDirectory, i),
+    outputProjectPath: path.join(outputDirectory, i),
+    outputLogPath: path.join(outputDirectory, i.replace(/\.sb3$/, '.txt')),
+  }));
 
-const validate = async () => {
-  throw new Error('TODO');
+const runTestcase = async (testcase) => {
+  const data = await fsPromises.readFile(testcase.inputPath);
+  const result = await sb3fix(data);
+  return {
+    fixedZip: new Uint8Array(result.fixedZip),
+    log: result.log.join('\n')
+  };
 };
 
-const generate = async () => {
-  throw new Error('TODO');
+const validate = async () => {
+  for (const testcase of testcases) {
+    await nodeTest(testcase, async (t) => {
+      const expectedFixedProject = new Uint8Array(await fsPromises.readFile(testcase.outputProjectPath));
+      const expectedLog = await fsPromises.readFile(testcase.outputLogPath, 'utf-8');
+      const result = await runTestcase(testcase);
 
-  for (const file of testFiles) {
-    const data = fs.readFileSync(path.join(samplesDirectory, file));
-    const fixed = await sb3fix(data);
-    console.log(fixed);
+      assert.equal(result.log, expectedLog, 'log');
+      assert.deepStrictEqual(result.fixedZip, expectedFixedProject, 'project');
+    });
   }
 };
 
+const update = async () => {
+  console.time('Updated snapshots');
+
+  await rimraf(outputDirectory);
+  await fsPromises.mkdir(outputDirectory, {
+    recursive: true
+  });
+
+  for (const testcase of testcases) {
+    console.log(`${testcase.name} ...`);
+    const result = await runTestcase(testcase);
+    await fsPromises.writeFile(testcase.outputProjectPath, result.fixedZip);
+    await fsPromises.writeFile(testcase.outputLogPath, result.log);
+  }
+
+  console.timeEnd('Updated snapshots');
+};
+
 const run = async () => {
-  if (process.argv.includes('--generate')) {
-    await generate();
+  if (process.argv.includes('--update')) {
+    await update();
   } else if (process.argv.includes('--validate')) {
     await validate();
   } else {
-    console.log('Please supply --generate or --validate');
+    console.log('Please supply --update or --validate');
     process.exit(1);
   }
 };
